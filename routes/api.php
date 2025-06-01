9d059be0c9bfbb791532616b8505452630603c63
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-06-01 13:18:20 +0700

    feat: implement patient dashboard api
    - Created PatientDashboardController to handle dashboard data retrieval.
    - Implemented logic to fetch active recommendations, last analysis, and favorite herbal data for the patient.
    - Added route for patient dashboard in api.php.
    - Included necessary models (Analysis, ConsumptionLog, Recommendation) and Carbon for date calculations.

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,23 @@
 use App\Http\Controllers\API\Auth\AuthController;
 use App\Http\Controllers\API\Patient\PatientAnalysisController;
 use App\Http\Controllers\API\Patient\PatientConsumptionLogController;
+use App\Http\Controllers\API\Patient\PatientDashboardController;
 use App\Http\Controllers\API\Patient\PatientUserProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('register', 'register')->name('api.register');
-        Route::post('login', 'login')->name('api.login');
-    });
+    Route::post('register', [AuthController::class, 'register'])->name('api.register');
+    Route::post('login', [AuthController::class, 'login'])->name('api.login');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
         Route::get('user', [AuthController::class, 'user'])->name('api.user');
 
         Route::prefix('patient')->name('patient.')->group(function () {
+            Route::apiResource('dashboard', PatientDashboardController::class)->only('index');
             Route::apiResource('analyses', PatientAnalysisController::class)->only(['index', 'store', 'show']);
-            Route::apiResource('consumption-logs', PatientConsumptionLogController::class)->only(['index']);
+            Route::apiResource('consumption-logs', PatientConsumptionLogController::class)->only('index');
             Route::patch('password', [PatientUserProfileController::class, 'updatePassword'])->name('profile.password');
             Route::apiResource('profile', PatientUserProfileController::class)->only(['show', 'update']);
         });
